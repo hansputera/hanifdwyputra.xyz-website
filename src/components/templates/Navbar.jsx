8214b62ed7092766522c3bc2fc077dbf4dfeef17
hansputera <hanifdwyputrasembiring@gmail.com>
2020-10-22 14:29:40 +0700

    added `/discord` on Navbar

--- src/components/templates/Navbar.jsx
+++ src/components/templates/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar_TEMPLATE() {
             <Nav className="collapse navbar-collapse mr-auto" id="navbarContent">
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/social">Social Media</Nav.Link>
+                <Nav.Link href="/discord">Discord</Nav.Link>
             </Nav>
         </Navbar>
     )
